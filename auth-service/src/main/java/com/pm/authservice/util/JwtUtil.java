517c1d65fa74b47261678be86b9df86978620b97
sudatra <mail2sudatra@gmail.com>
2025-08-21 09:47:52 +0530

    added token generation

--- auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -1,12 +1,16 @@
 package com.pm.authservice.util;
 
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Base64;
+import java.util.Date;
 
+@Component
 public class JwtUtil {
     private final Key secretKey;
 
@@ -17,5 +21,17 @@ public class JwtUtil {
         ;
 
         this.secretKey = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(String email, String role) {
+        return Jwts
+                .builder()
+                .subject(email)
+                .claim("role", role)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(secretKey)
+                .compact()
+        ;
     }
 }
